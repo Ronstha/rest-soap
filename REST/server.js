c0fb00af8347dda94f6a5f51093f1e21261c018c
Ronish Shrestha <ronish.sht2015@gmail.com>
2025-02-16 15:18:10 +0545

    commit

--- REST/server.js
+++ REST/server.js
@@ -5,11 +5,13 @@ app.post('/calculate',(req,res)=>{
     const {a,b}=req.body
     if(!a||!b){
         res.status(400).json({'message':"Invalid Data"})
+        return
     }
     res.status(200).json({
         sum:a+b,
         difference:a-b
     })
+    return
 })
 
 
